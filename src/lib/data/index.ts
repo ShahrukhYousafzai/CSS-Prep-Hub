4940d5d1b16091849e4c9bdf9ca38b21bf686c75
Shahrukh Yousafzai <shahrukhhkhan2003@gmail.com>
2025-08-09 23:32:20 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -1,7 +1,7 @@
 
 import type { PastPaper, Quiz, InterviewQuestion, PsychTest, SubjectWiseQuestion } from '../types';
 import { pastPapers as pastPapersCompulsory } from './past-papers/compulsory';
-import { pastPapers as pastPapersGroup1 } from './past-papers/group-1/index';
+import { pastPapers as pastPapersGroup1 } from './past-papers/group-1';
 import { pastPapers as pastPapersGroup2 } from './past-papers/group-2';
 import { pastPapers as pastPapersGroup3 } from './past-papers/group-3';
 import { pastPapers as pastPapersGroup4 } from './past-papers/group-4';
